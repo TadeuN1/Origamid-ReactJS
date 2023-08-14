13102a3bbbd681ed19e5cfee635a8363aebb9176
Tadeu Neres <tadeunrs@gmail.com>
2023-08-14 18:00:41 -0300

    useState Lesson

--- reactHooks-03/useState/src/App.jsx
+++ reactHooks-03/useState/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
 
 const App = () => {
-  const [modal, setModal] = React.useState(false);
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['Item 1']);
+  function handleClick() {
+    setContar(contar + 1);
+    setItems([...items, 'Item ' + (contar + 1)]);
+  }
   return (
     <div>
-      <div>{modal ? 'Modal Aberto' : 'Modal Fechado'}</div>
-      <ButtonModal setModal={setModal} />
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+      <button onClick={handleClick}>{contar}</button>
     </div>
   );
 };
